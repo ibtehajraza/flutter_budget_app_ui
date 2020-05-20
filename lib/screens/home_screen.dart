a75249cdb6ecf5bc9d8efe34195e3c8bfdf0dcd1
ibtehajraza <ibtahajraza@gmail.com>
2020-05-20 22:41:23 +0300

    Added an app bar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Simple Budget'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
